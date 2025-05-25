e604e5df97dc6b7355eab4b5ca6e5232775268ed
Roman Makeev <makeevrserg@gmail.com>
2025-05-26 00:16:50 +0300

    add appbar into menu screen

--- modules/features/info/ui/src/commonMain/kotlin/com/makeevrserg/empireprojekt/mobile/features/ui/info/InfoScreen.kt
+++ modules/features/info/ui/src/commonMain/kotlin/com/makeevrserg/empireprojekt/mobile/features/ui/info/InfoScreen.kt
@@ -17,11 +17,14 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bolt
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.tooling.preview.Preview
 import com.makeevrserg.empireprojekt.mobile.buildkonfig.BuildKonfig
 import com.makeevrserg.empireprojekt.mobile.core.resources.MR
+import com.makeevrserg.empireprojekt.mobile.core.ui.appbar.AstraCenterAlignedTopAppBar
 import com.makeevrserg.empireprojekt.mobile.core.ui.common.navBarsPadding
 import com.makeevrserg.empireprojekt.mobile.core.ui.option.OptionHref
 import com.makeevrserg.empireprojekt.mobile.core.ui.option.OptionInfo
@@ -48,134 +51,137 @@ fun InfoScreen(
     onWikiClick: () -> Unit
 ) {
     val models = remember { InfoScreenLinks.get() }
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.primaryVariant)
-            .padding(horizontal = AppTheme.dimens.XS),
-        verticalArrangement = Arrangement.spacedBy(AppTheme.dimens.XS)
     ) {
-        item { Spacer(Modifier.systemBarsPadding()) }
-        item {
-            OptionSection(modifier = Modifier) {
+        AstraCenterAlignedTopAppBar(title = "МЕНЮ")
+        LazyColumn(
+            modifier = Modifier.padding(horizontal = AppTheme.dimens.XS),
+            verticalArrangement = Arrangement.spacedBy(AppTheme.dimens.XS, Alignment.Top)
+        ) {
+            item {
+                OptionSection(modifier = Modifier) {
+                    Column {
+                        OptionHref(
+                            text = "Towns",
+                            onClick = {
+                                onTownsClick.invoke()
+                            },
+                            icon = MR.images.ic_town.asPainter(),
+                            contentPadding = PaddingValues(
+                                horizontal = AppTheme.dimens.XS,
+                                vertical = AppTheme.dimens.XS
+                            ),
+                        )
+                        OptionSeparator(Modifier.fillMaxWidth())
+                        OptionHref(
+                            text = "Ratings",
+                            onClick = {
+                                onRatingsClick.invoke()
+                            },
+                            icon = MR.images.ic_people.asPainter(),
+                            contentPadding = PaddingValues(
+                                horizontal = AppTheme.dimens.XS,
+                                vertical = AppTheme.dimens.XS
+                            ),
+                        )
+                        OptionSeparator(Modifier.fillMaxWidth())
+                        OptionHref(
+                            text = "Votes",
+                            onClick = {
+                                onVotesClick.invoke()
+                            },
+                            icon = MR.images.ic_vote.asPainter(),
+                            contentPadding = PaddingValues(
+                                horizontal = AppTheme.dimens.XS,
+                                vertical = AppTheme.dimens.XS
+                            ),
+                        )
+                        OptionSeparator(Modifier.fillMaxWidth())
+                        OptionHref(
+                            text = "WIKI",
+                            onClick = {
+                                onWikiClick.invoke()
+                            },
+                            icon = MR.images.ic_knowledge.asPainter(),
+                            contentPadding = PaddingValues(
+                                horizontal = AppTheme.dimens.XS,
+                                vertical = AppTheme.dimens.XS
+                            ),
+                        )
+                        OptionSeparator(Modifier.fillMaxWidth())
+                        OptionSwitch(
+                            text = "Темная тема",
+                            onCheckChange = {
+                                onThemeToggle.invoke()
+                            },
+                            checked = AppTheme.astraColors.isDark,
+                            icon = MR.images.ic_theme.asPainter(),
+                            contentPadding = PaddingValues(
+                                horizontal = AppTheme.dimens.XS,
+                                vertical = AppTheme.dimens.XS
+                            ),
+                        )
+                    }
+                }
+            }
+
+            item {
                 Column {
-                    OptionHref(
-                        text = "Towns",
-                        onClick = {
-                            onTownsClick.invoke()
-                        },
-                        icon = MR.images.ic_town.asPainter(),
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
+                    Text(
+                        text = IR.strings.info_more_links.asComposableString(),
+                        style = MaterialTheme.typography.h5,
+                        color = MaterialTheme.colors.onPrimary,
+                        fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
                     )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionHref(
-                        text = "Ratings",
-                        onClick = {
-                            onRatingsClick.invoke()
-                        },
-                        icon = MR.images.ic_people.asPainter(),
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionHref(
-                        text = "Votes",
-                        onClick = {
-                            onVotesClick.invoke()
-                        },
-                        icon = MR.images.ic_vote.asPainter(),
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionHref(
-                        text = "WIKI",
-                        onClick = {
-                            onWikiClick.invoke()
-                        },
-                        icon = MR.images.ic_knowledge.asPainter(),
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionSwitch(
-                        text = "Темная тема",
-                        onCheckChange = {
-                            onThemeToggle.invoke()
-                        },
-                        checked = AppTheme.astraColors.isDark,
-                        icon = MR.images.ic_theme.asPainter(),
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
+                    Text(
+                        text = "All this links associated with EmpireProjekt and AstraInteractive. " +
+                                "This text specially displayed for google play " +
+                                "support which considering this links as advertisement.",
+                        style = MaterialTheme.typography.overline,
+                        color = MaterialTheme.colors.onPrimary,
+                        fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
                     )
                 }
             }
-        }
 
-        item {
-            Column {
-                Text(
-                    text = IR.strings.info_more_links.asComposableString(),
-                    style = MaterialTheme.typography.h5,
-                    color = MaterialTheme.colors.onPrimary,
-                    fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
-                )
-                Text(
-                    text = "All this links associated with EmpireProjekt and AstraInteractive. " +
-                        "This text specially displayed for google play " +
-                        "support which considering this links as advertisement.",
-                    style = MaterialTheme.typography.overline,
-                    color = MaterialTheme.colors.onPrimary,
-                    fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
-                )
-            }
-        }
-
-        item {
-            OptionSection(modifier = Modifier) {
-                Column {
-                    models.forEachIndexed { i, linkModel ->
-                        LinkWidget(
-                            linkBrowser = linkBrowser,
-                            linkModel = linkModel
-                        )
-                        if (i != models.lastIndex) {
-                            OptionSeparator(Modifier.fillMaxWidth())
+            item {
+                OptionSection(modifier = Modifier) {
+                    Column {
+                        models.forEachIndexed { i, linkModel ->
+                            LinkWidget(
+                                linkBrowser = linkBrowser,
+                                linkModel = linkModel
+                            )
+                            if (i != models.lastIndex) {
+                                OptionSeparator(Modifier.fillMaxWidth())
+                            }
                         }
                     }
                 }
             }
-        }
-        item {
-            OptionSection(modifier = Modifier) {
-                OptionInfo(
-                    modifier = Modifier.padding(vertical = AppTheme.dimens.XS),
-                    icon = rememberVectorPainter(Icons.Filled.Bolt),
-                    text = "Version",
-                    endText = "${BuildKonfig.VERSION_CODE} (${BuildKonfig.VERSION_NAME})",
-                    contentPadding = PaddingValues(
-                        horizontal = AppTheme.dimens.XS,
-                        vertical = AppTheme.dimens.XS
-                    ),
-                )
+            item {
+                OptionSection(modifier = Modifier) {
+                    OptionInfo(
+                        modifier = Modifier.padding(vertical = AppTheme.dimens.XS),
+                        icon = rememberVectorPainter(Icons.Filled.Bolt),
+                        text = "Version",
+                        endText = "${BuildKonfig.VERSION_CODE} (${BuildKonfig.VERSION_NAME})",
+                        contentPadding = PaddingValues(
+                            horizontal = AppTheme.dimens.XS,
+                            vertical = AppTheme.dimens.XS
+                        ),
+                    )
+                }
             }
-        }
-        item {
-            Spacer(modifier = Modifier.navBarsPadding())
-        }
-        item {
-            Spacer(modifier = Modifier.height(AppTheme.dimens.L))
+            item {
+                Spacer(modifier = Modifier.navBarsPadding())
+            }
+            item {
+                Spacer(modifier = Modifier.height(AppTheme.dimens.L))
+            }
         }
     }
 }
