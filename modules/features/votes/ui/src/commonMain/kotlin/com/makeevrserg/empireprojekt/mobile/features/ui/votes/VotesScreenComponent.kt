9e31919296e141b0e67ce18d11183e290668727c
Roman Makeev <makeevrserg@gmail.com>
2025-06-19 16:37:11 +0300

    add votes

--- modules/features/votes/ui/src/commonMain/kotlin/com/makeevrserg/empireprojekt/mobile/features/ui/votes/VotesScreenComponent.kt
+++ modules/features/votes/ui/src/commonMain/kotlin/com/makeevrserg/empireprojekt/mobile/features/ui/votes/VotesScreenComponent.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -26,17 +27,81 @@ import com.makeevrserg.empireprojekt.mobile.core.ui.theme.AdaptThemeFade
 import com.makeevrserg.empireprojekt.mobile.core.ui.theme.AppTheme
 import com.makeevrserg.empireprojekt.mobile.core.ui.util.asFontFamily
 import com.makeevrserg.empireprojekt.mobile.core.ui.util.asPainter
+import dev.icerock.moko.resources.ImageResource
+
+data class VoteUrl(
+    val name: String,
+    val url: String,
+    val hasPrize: Boolean,
+    val image: ImageResource
+)
 
 @Composable
 fun VotesScreenComponent(
     onPop: () -> Unit,
     onClick: (String) -> Unit
 ) {
+    val voteUrls = listOf(
+        VoteUrl(
+            name = "FrontierSmp",
+            url = "https://minecraftrating.ru/vote/290898/",
+            hasPrize = false,
+            image = MR.images.ic_minecraftrating
+        ),
+        VoteUrl(
+            name = "WildernessSmp",
+            url = "https://minecraftrating.ru/vote/295288/",
+            hasPrize = false,
+            image = MR.images.ic_minecraftrating
+        ),
+        VoteUrl(
+            name = "EndureCraft",
+            url = "https://minecraftrating.ru/vote/295287/",
+            hasPrize = false,
+            image = MR.images.ic_minecraftrating
+        ),
+        VoteUrl(
+            name = "ExploreSmp",
+            url = "httpsc://minecraftrating.ru/vote/293843/",
+            hasPrize = false,
+            image = MR.images.ic_minecraftrating
+        ),
+        VoteUrl(
+            name = "LongExile",
+            url = "https://minecraftrating.ru/vote/290896/",
+            hasPrize = false,
+            image = MR.images.ic_minecraftrating
+        ),
+        VoteUrl(
+            url = "https://minecraftrating.ru/projects/empireprojekt/",
+            hasPrize = true,
+            image = MR.images.ic_minecraftrating,
+            name = "Server Project"
+        ),
+        VoteUrl(
+            name = "FrontierSurvival",
+            url = "https://hotmc.ru/minecraft-server-200387/",
+            hasPrize = true,
+            image = MR.images.ic_hotmc
+        ),
+        VoteUrl(
+            name = "WildernessSmp",
+            url = "https://hotmc.ru/minecraft-server-258775/",
+            hasPrize = true,
+            image = MR.images.ic_hotmc
+        ),
+        VoteUrl(
+            name = "EndureCraft",
+            url = "https://hotmc.ru/minecraft-server-258776/",
+            hasPrize = true,
+            image = MR.images.ic_hotmc
+        ),
+    )
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
             .background(MaterialTheme.colors.primaryVariant)
-            .padding(horizontal = AppTheme.dimens.XS),
+            .padding(horizontal = AppTheme.dimens.S),
         topBar = {
             AstraCenterAlignedTopAppBar(
                 title = "ГОЛОСОВАНИЕ",
@@ -55,59 +120,46 @@ fun VotesScreenComponent(
             )
             Text(
                 text = "На этой странице вы можете помочь серверу привлечь новых игроков проголосовав за него на других мониторингах\n" +
-                    "Эта страница сделана таким образом, чтобы голосовать можно было максимально быстро и легко\n" +
-                    "Просто нажмите на один вариант, а лучше на все.\n" +
-                    "В появившемся окне проголосуйте за сервер",
+                        "Просто нажмите на один вариант, а лучше на все.\n",
                 style = MaterialTheme.typography.body1,
                 color = MaterialTheme.colors.onSecondary,
                 fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
             )
             Spacer(Modifier.height(AppTheme.dimens.XS))
-            OptionSection(modifier = Modifier) {
-                Column {
-                    OptionHref(
-                        text = "Главный сервер",
-                        onClick = {
-                            onClick.invoke("https://minecraftrating.ru/vote/290898/")
-                        },
-                        endText = "1.21",
-                        icon = MR.images.ainteractivelogo.asPainter(),
-                        iconTint = Color.Unspecified,
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionHref(
-                        text = "Главный сервер",
-                        endText = "1.21.4",
-                        onClick = {
-                            onClick.invoke("https://minecraftrating.ru/vote/293843/")
-                        },
-                        icon = MR.images.ainteractivelogo.asPainter(),
-                        iconTint = Color.Unspecified,
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
-                    OptionSeparator(Modifier.fillMaxWidth())
-                    OptionHref(
-                        text = "Дополнительный 1.21",
-                        onClick = {
-                            onClick.invoke("https://minecraftrating.ru/vote/290896/")
-                        },
-                        endText = "1.21",
-                        icon = MR.images.img_head_error.asPainter(),
-                        iconTint = Color.Unspecified,
-                        contentPadding = PaddingValues(
-                            horizontal = AppTheme.dimens.XS,
-                            vertical = AppTheme.dimens.XS
-                        ),
-                    )
+            voteUrls.groupBy { it.hasPrize }
+                .forEach { (hasPrize, voteUrls) ->
+                    if (hasPrize) {
+                        Spacer(Modifier.height(AppTheme.dimens.XS))
+                        Text(
+                            text = "При голосовании за эти сервера вы получите награду!",
+                            style = MaterialTheme.typography.body1,
+                            color = MaterialTheme.colors.onSecondary,
+                            fontFamily = MR.fonts.jetbrainsmono_wght.asFontFamily()
+                        )
+                        Spacer(Modifier.height(AppTheme.dimens.XS))
+                    }
+                    OptionSection(modifier = Modifier) {
+                        Column {
+                            voteUrls.forEachIndexed { index, voteUrl ->
+                                OptionHref(
+                                    text = voteUrl.name,
+                                    onClick = {
+                                        onClick.invoke(voteUrl.url)
+                                    },
+                                    icon = voteUrl.image.asPainter(),
+                                    iconTint = Color.Unspecified,
+                                    contentPadding = PaddingValues(
+                                        horizontal = AppTheme.dimens.XS,
+                                        vertical = AppTheme.dimens.XS
+                                    ),
+                                )
+                                if (index != voteUrls.lastIndex) {
+                                    OptionSeparator(Modifier.fillMaxWidth())
+                                }
+                            }
+                        }
+                    }
                 }
-            }
             Spacer(Modifier.navBarsPadding())
         }
     }
