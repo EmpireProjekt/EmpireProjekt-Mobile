6847c181436db47398c43f56ca78c2ba1cc81ce5
Roman Makeev <makeevrserg@gmail.com>
2026-09-25 15:59:33 +0300

    [REFACTOR] Split the wear main screen and hoist the theme chip state

--- instances/wearApp/src/main/kotlin/com/makeevrserg/empireprojekt/mobile/wear/features/main/ui/MainScreen.kt
+++ instances/wearApp/src/main/kotlin/com/makeevrserg/empireprojekt/mobile/wear/features/main/ui/MainScreen.kt
@@ -1,23 +1,10 @@
 package com.makeevrserg.empireprojekt.mobile.wear.features.main.ui
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.wear.compose.material.Scaffold
-import com.makeevrserg.empireprojekt.mobile.R
-import com.makeevrserg.empireprojekt.mobile.core.ui.theme.AppTheme
 import com.makeevrserg.empireprojekt.mobile.features.theme.presentation.ThemeSwitcherComponent
-import com.makeevrserg.empireprojekt.mobile.wear.features.main.ui.components.NavChip
-import com.makeevrserg.empireprojekt.mobile.wear.features.main.ui.components.ThemeChip
 
 @Composable
 fun MainScreen(
@@ -26,30 +13,12 @@ fun MainScreen(
     onOpenPingClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Scaffold(
-        modifier = modifier.background(MaterialTheme.colors.primaryVariant),
-        positionIndicator = {
-        }
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(horizontal = AppTheme.dimens.M),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Spacer(modifier = Modifier.height(AppTheme.dimens.S))
-            ThemeChip(themeSwitcherComponent = themeSwitcherComponent)
-            Spacer(modifier = Modifier.height(AppTheme.dimens.S))
-            NavChip(
-                text = stringResource(R.string.wear_main_statuses_action),
-                onClick = onOpenStatusesClick
-            )
-            Spacer(modifier = Modifier.height(AppTheme.dimens.S))
-            NavChip(
-                text = stringResource(R.string.wear_main_ping_action),
-                onClick = onOpenPingClick
-            )
-        }
-    }
+    val theme by themeSwitcherComponent.theme.collectAsState()
+    MainComposableScreen(
+        theme = theme,
+        onThemeClick = themeSwitcherComponent::next,
+        onOpenStatusesClick = onOpenStatusesClick,
+        onOpenPingClick = onOpenPingClick,
+        modifier = modifier
+    )
 }
--- instances/wearApp/src/main/kotlin/com/makeevrserg/empireprojekt/mobile/wear/features/main/ui/components/ThemeChip.kt
+++ instances/wearApp/src/main/kotlin/com/makeevrserg/empireprojekt/mobile/wear/features/main/ui/components/ThemeChip.kt
@@ -3,25 +3,25 @@ package com.makeevrserg.empireprojekt.mobile.wear.features.main.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.makeevrserg.empireprojekt.mobile.R
 import com.makeevrserg.empireprojekt.mobile.core.resources.MR
 import com.makeevrserg.empireprojekt.mobile.core.resources.ic_bedtime
 import com.makeevrserg.empireprojekt.mobile.core.resources.ic_sunny
+import com.makeevrserg.empireprojekt.mobile.core.ui.theme.AdaptThemeFade
+import com.makeevrserg.empireprojekt.mobile.core.ui.theme.ComposeTheme
 import com.makeevrserg.empireprojekt.mobile.core.ui.util.asPainter
 import com.makeevrserg.empireprojekt.mobile.features.theme.data.model.Theme
-import com.makeevrserg.empireprojekt.mobile.features.theme.presentation.ThemeSwitcherComponent
 import com.makeevrserg.empireprojekt.mobile.wear.features.components.IconTextChip
 
 @Composable
 fun ThemeChip(
-    themeSwitcherComponent: ThemeSwitcherComponent,
+    theme: Theme,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    val theme by themeSwitcherComponent.theme.collectAsState()
     val icon = when (theme) {
         Theme.DARK -> MR.images.ic_bedtime
         Theme.LIGHT -> MR.images.ic_sunny
@@ -31,6 +31,22 @@ fun ThemeChip(
         painter = icon.asPainter(),
         modifier = modifier.fillMaxWidth(),
         iconColor = MaterialTheme.colors.onPrimary,
-        onClick = themeSwitcherComponent::next
+        onClick = onClick
     )
+}
+
+@Preview
+@Composable
+private fun ThemeChipDarkPreview() {
+    AdaptThemeFade(composeTheme = ComposeTheme.DARK) {
+        ThemeChip(theme = Theme.DARK, onClick = {})
+    }
+}
+
+@Preview
+@Composable
+private fun ThemeChipLightPreview() {
+    AdaptThemeFade(composeTheme = ComposeTheme.LIGHT) {
+        ThemeChip(theme = Theme.LIGHT, onClick = {})
+    }
 }
